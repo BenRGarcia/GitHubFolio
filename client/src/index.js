fdb3868f1003b9e5c11d77a0006fe6f9c2997f50
Ben Garcia <ben@seebenprogram.com>
2018-06-09 21:31:56 -0400

    import bootstrap, jquery, enable tooltips/popovers

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import $ from 'jquery';
+require('./vendor/bootstrap/bootstrap.min.css');
+require('bootstrap');
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
