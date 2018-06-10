f34f2973aaf0b43f419c5fbcedc084f2ca7f5589
Ben Garcia <ben@seebenprogram.com>
2018-06-09 21:08:13 -0400

    remove boilerplate test

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,20 +1,7 @@
 const router = require('express').Router()
-const user = require('./user')
-var db = require('../../models')
-var isAuthenticated = require('./utils/isAuthenticated.js')
+const userRoute = require('./user')
 
 // API Routes - '/api/...'
-router.route('/user/data')
-  .get(/* isAuthenticated, */(req, res, next) => {
-    res.json({ msg: 'api server received `GET` request' })
-  })
-
-  .post(/* isAuthenticated, */(req, res, next) => {
-    res.json({ msg: 'api server received `POST` request' })
-  })
-
-  .put(/* isAuthenticated, */(req, res, next) => {
-    res.json({ msg: 'api server received `PUT` request' })
-  })
+router.use('/user', userRoute)
 
 module.exports = router
