317ae9ad2ccceb4de76421c6e008b3f7c320cf95
Ben Garcia <ben@seebenprogram.com>
2018-06-09 20:11:13 -0400

    add mock api endpoints

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
-const exampleRoutes = require('./exampleRoute')
+const user = require('./user')
+var db = require('../../models')
+var isAuthenticated = require('./utils/isAuthenticated.js')
 
 // API Routes - '/api/...'
-router.use('/example', exampleRoutes)
+router.route('/user/data')
+  .get(/* isAuthenticated, */(req, res, next) => {
+    res.json({ msg: 'api server received `GET` request' })
+  })
+
+  .post(/* isAuthenticated, */(req, res, next) => {
+    res.json({ msg: 'api server received `POST` request' })
+  })
+
+  .put(/* isAuthenticated, */(req, res, next) => {
+    res.json({ msg: 'api server received `PUT` request' })
+  })
 
 module.exports = router
