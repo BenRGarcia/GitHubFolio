d5cae96aabb5c08ba316b09a2c1be4c21d143aad
Ben Garcia <ben@seebenprogram.com>
2018-06-09 21:08:34 -0400

    add pkg imports

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const createError = require('http-errors')
 const express = require('express')
+const helmet = require('helmet')
+const session = require('express-session')
+const RedisStore = require('connect-redis')(session)
+const redis = require('redis')
+const client = redis.createClient(process.env.REDIS_URL || null)
+// const passport = require('./config/passport.js')
+const limiterConfig = require('./config/limiter.js')
+const sessionConfig = require('./config/session')(RedisStore, client)
 const path = require('path')
 const logger = require('morgan')
 const app = express()
 const routes = require('./routes')
+const limiter = require('express-limiter')(app, client)
+limiter(limiterConfig)
 
 // Middleware
+app.use(helmet())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(session(sessionConfig))
+// app.use(passport.initialize())
+// app.use(passport.session())
 // Static assets - React Build
 app.use(express.static(path.join(__dirname, 'build')))
 // API Server routes
