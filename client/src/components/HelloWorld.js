ae713df052e4b2b774601a76e0c54cad6067b9b6
Ben Garcia <ben@seebenprogram.com>
2018-06-09 21:31:38 -0400

    add mock bootstrap components

--- client/src/components/HelloWorld.js
+++ client/src/components/HelloWorld.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 export const HelloWorld = props => (
   <div>
-    <h1>{props.title}</h1>
+    <h1 className='display-2'>{props.title}</h1>
     <p>{props.apiServerStatus}</p>
+    <button
+      type="button"
+      className="btn btn-secondary"
+      data-toggle="tooltip"
+      data-placement="top"
+      title="It Works!"
+    >
+      Tooltip
+    </button>
+    <br />
+    <button
+      type="button"
+      className="btn btn-lg btn-danger"
+      data-toggle="popover"
+      title="Popover works, too"
+      data-content="Magic...."
+    >
+      Popover
+    </button>
   </div>
 );
