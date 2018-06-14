7879daba279a0be0d3e0ccea4dc58fa5fd9ea0d0
Ben Garcia <ben@seebenprogram.com>
2018-06-14 10:41:24 -0400

    add redis/passport/session mounts

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const session = require('express-session')
 const RedisStore = require('connect-redis')(session)
 const redis = require('redis')
 const client = redis.createClient(process.env.REDIS_URL || null)
-// const passport = require('./config/passport.js')
+const passport = require('./config/passport.js')
 const limiterConfig = require('./config/limiter.js')
 const sessionConfig = require('./config/session')(RedisStore, client)
 const path = require('path')
@@ -21,12 +21,17 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(session(sessionConfig))
-// app.use(passport.initialize())
-// app.use(passport.session())
+app.use(passport.initialize())
+app.use(passport.session())
 // Static assets - React Build
 app.use(express.static(path.join(__dirname, 'build')))
 // API Server routes
 app.use(routes)
+
+// Handle redirects
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => next(createError(404)))
