89058b6246dfc445ce87c77016bd1c9b3903466d
Ben Garcia <ben@seebenprogram.com>
2018-06-14 13:19:53 -0400

    refactor for readability

--- config/passportGitHub.js
+++ config/passportGitHub.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-var GitHubStrategy = require('passport-github').Strategy
+const GitHubStrategy = require('passport-github').Strategy
 const User = require('../controllers/UserController')
 
 // https://github.com/jaredhanson/passport-github
@@ -11,19 +11,21 @@ const strategyConfig = {
 }
 
 const verifyCb = async (accessToken, refreshToken, profile, cb) => {
-  // Destructure GitHub profile oAuth response object
-  const { displayName, profileUrl } = profile
-  const gitHubId = profile.id
-  const email = profile.emails[0].value
-  const photo = profile.photos[0].value
-  const { bio, location } = profile._json
-  console.log(`\n=====stuff:\n`, gitHubId, displayName, profileUrl, email, photo, bio, location)
+  // Compose user object from GitHub oAuth `profile` response
+  const userProfile = {
+    gitHubId: profile.id,
+    displayName: profile.displayName,
+    profileUrl: profile.profileUrl,
+    accessToken: accessToken,
+    email: profile.emails[0].value,
+    photo: profile.photos[0].value,
+    bio: profile._json.bio,
+    location: profile._json.location
+  }
   // Find or create user with destructured data
-  User.loginFindOrCreate({ gitHubId, displayName, profileUrl, accessToken, email, photo, bio, location })
-    .then(user => {
-      console.log(`user:`, user)
-      return cb(null, user)
-    })
+  const user = await User.loginFindOrCreate(userProfile)
+  // Passport.js callback
+  return cb(null, user._id)
 }
 
 const gitHubStrategy = new GitHubStrategy(strategyConfig, verifyCb)
