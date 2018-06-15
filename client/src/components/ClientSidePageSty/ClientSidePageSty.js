79f3ed48d446fc9aebdf8e066e9289d9f1b7f239
Melissa Burnham <melissarburnham@gmail.com>
2018-06-14 20:37:29 -0400

    stylized page component created

---
+++ client/src/components/ClientSidePageSty/ClientSidePageSty.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./ClientSidePageSty.css";
+import { MinimalistPinnedCard } from '../../components/MinimalistPinnedCard/MinimalistPinnedCard';
+
+
+export const ClientSidePageSty = props => {
+  const {userInfo, pinnedRepos} = props;
+  return(
+  <div className= "minPageContainer">
+    <div className="container pt-4">
+      <div className="section pt-4 pb-4">
+        <div className="titlefont pt-4 pb-4">
+            <a>Minimalist</a>
+        </div>
+      </div>
+      <MinimalistPinnedCard pinnedRepos={pinnedRepos} />
+      <div className='pt-4 mt-4'>
+        <a>Powered by GitHubFolio</a>
+      </div>
+    </div>
+  </div>
+  )
+}
