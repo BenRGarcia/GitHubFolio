8162d08bf91d87df61896ba6466c2c9e79b81b4a
Ben Garcia <ben@seebenprogram.com>
2018-06-16 13:05:54 -0400

    Add accessToken sanitization

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,12 @@
 const db = require('../models')
 
+// Remove accessToken from response object
+const returnSanitizedObject = resp => {
+  resp = resp.toObject()
+  delete resp.accessToken
+  return resp
+}
+
 const loginFindOrCreate = async ({
   gitHubId,
   accessToken,
@@ -13,16 +20,20 @@ const loginFindOrCreate = async ({
   const userDoesExist = await db.User.findOne({ gitHubId })
   return userDoesExist
     ? db.User.findOneAndUpdate({ _id: userDoesExist._id }, { displayName, profileUrl, accessToken, email, photo, bio, location })
+      .then(res => returnSanitizedObject(res))
     : db.User.create({ gitHubId, displayName, profileUrl, accessToken, email, photo, bio, location })
+      .then(res => returnSanitizedObject(res))
 }
 
 const findOne = async ({ _id }) => {
   return db.User.findOne({ _id }).populate('pinnedRepositories')
+    .then(res => returnSanitizedObject(res))
 }
 
 const findOneAndUpdate = async ({ _id }, { displayName, profileUrl, email, photo, bio, location, template, color }) => {
   const userData = { displayName, profileUrl, email, photo, bio, location, template, color }
   return db.User.findOneAndUpdate({ _id }, userData)
+    .then(res => returnSanitizedObject(res))
 }
 
 const setPinnedRepos = async ({ _id }, repoIdArray) => {
