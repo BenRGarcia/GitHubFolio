f71f0823ff4ac1dab7ab15103a8e58d6b1b131b0
Ben Garcia <ben@seebenprogram.com>
2018-06-16 15:19:53 -0400

    add href to logout endpoint

--- client/src/components/DashboardSidebar/DashboardSidebar.js
+++ client/src/components/DashboardSidebar/DashboardSidebar.js
@@ -10,7 +10,7 @@ export const DashboardSidebar = props => (
       <a href="/dashboard/user" className='sidenavText'>Edit User Info<i className="fas fa-caret-right arrowPad"></i></a><hr />
       <a href="/dashboard/repos" className='sidenavText'>Edit Repo Info<i className="fas fa-caret-right arrowPad"></i></a><hr />      
       <a href="/dashboard/preview" className='sidenavText'>Preview Portfolio<i className="fas fa-caret-right arrowPad"></i></a><hr />
-      <a href="/" className='sidenavText'>Logout</a>
+      <a href="/api/auth/logout" className='sidenavText'>Logout</a>
     </div>
   </div>
 );
