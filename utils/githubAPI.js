ffe9cd40f6b064c77f5e2df7a20d021c659968a2
Ben Garcia <ben@seebenprogram.com>
2018-06-16 16:26:23 -0400

    bug fix, refactor with try catch block

--- utils/githubAPI.js
+++ utils/githubAPI.js
@@ -1,27 +1,23 @@
 const axios = require('axios')
-const graphqlQuery = `
-  query {
-    viewer {
-      pinnedRepositories(first:6) {
-        edges { node { name description homepageUrl url } }
-      }
-    }
-  }
-`
+const graphqlQuery = `{ viewer { pinnedRepositories(first: 6) { edges { node { name description homepageUrl url } } } } }`
 
-export const getPinnedRepos = async accessToken => {
-  // Get user's pinned repo data from GitHub
-  const res = await axios({
-    method: 'POST',
-    url: 'https://api.github.com/graphql',
-    data: JSON.stringify({ graphqlQuery }),
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `bearer ${accessToken}`
-    }
-  })
-  // Return normalized data
-  return normalizePinnedRepos(res.data.data.viewer.pinnedRepositories.edges)
+const getPinnedRepos = async accessToken => {
+  try {
+    // Get user's pinned repo data from GitHub
+    const res = await axios({
+      method: 'POST',
+      url: 'https://api.github.com/graphql',
+      data: { query: graphqlQuery },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `bearer ${accessToken}`
+      }
+    })
+    // Return normalized data
+    return normalizePinnedRepos(res.data.data.viewer.pinnedRepositories.edges)
+  } catch (err) {
+    return err
+  }
 }
 
 const normalizePinnedRepos = repoArray => {
@@ -29,4 +25,8 @@ const normalizePinnedRepos = repoArray => {
     const { name, description, homepageUrl, url } = repo.node
     return { name, description, homepageUrl, url }
   })
+}
+
+module.exports = {
+  getPinnedRepos
 }
