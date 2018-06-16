8227c8304eb898bd28dbe90a747423e842a2464b
Ben Garcia <ben@seebenprogram.com>
2018-06-16 13:42:45 -0400

    add deleteOldRepos method

--- controllers/PinnedRepositoryController.js
+++ controllers/PinnedRepositoryController.js
@@ -1,7 +1,24 @@
 const db = require('../models')
 const User = require('./UserController')
 
+const deleteOldRepos = async ({ _id }) => {
+  const resp = await User.getRepoIds({ _id })
+  const repoIds = resp.pinnedRepositories
+  if (repoIds.length > 0) {
+    const mongoDeliverable = repoIds.map(repoId => {
+      return {
+        deleteOne: {
+          filter: { _id: repoId }
+        }
+      }
+    })
+    return db.PinnedRepositories.bulkWrite(mongoDeliverable)
+  }
+  return false
+}
+
 const bulkCreate = async ({ _id }, arrayOfObjects) => {
+  await deleteOldRepos({ _id })
   const mongoDeliverable = arrayOfObjects.map(pinnedRepo => {
     const { name, description, homepageUrl, url } = pinnedRepo
     return {
