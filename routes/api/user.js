dd14bdc949e3419595918c388eef1ac06fc1e8b5
Ben Garcia <ben@seebenprogram.com>
2018-06-16 16:25:13 -0400

    update user id access from req

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,25 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const { getPinnedRepos } = require('../../utils/githubAPI')
 const isAuthenticated = require('../../utils/isAuthenticated')
 const User = require('../../controllers/UserController')
 const PinnedRepos = require('../../controllers/PinnedRepositoryController')
 
 /**
  *  Helpful hint:
- *    `req.user` is the user's MongoDB `_id`
+ *    `req.user._id` is the user's MongoDB `_id`
  */
 
 // API Routes - '/api/user'
 router.route('/data')
   .get(isAuthenticated, (req, res, next) => {
-    const _id = req.user
+    const _id = req.user._id
     User.findOne({ _id })
       .then(userData => res.json(userData))
       .catch(err => next(err))
   })
 
   .put(isAuthenticated, (req, res, next) => {
-    const _id = req.user
+    const _id = req.user._id
     const { displayName, profileUrl, email, photo, bio, location, template, color } = req.body
     const updateObject = { displayName, profileUrl, email, photo, bio, location, template, color }
     User.findOneAndUpdate({ _id }, updateObject)
@@ -28,8 +29,17 @@ router.route('/data')
   })
 
 router.route('/pinnedrepos')
+  .get(isAuthenticated, (req, res, next) => {
+    // Use githubAPI module to query v4 graphQL endpoint
+    getPinnedRepos(req.user.accessToken)
+      .then(resp => {
+        // Send JSON object of pinned repo data
+        res.json(resp)
+      })
+  })
+
   .post(isAuthenticated, (req, res, next) => {
-    const _id = req.user
+    const _id = req.user._id
     const repos = req.body
     PinnedRepos.bulkCreate({ _id }, repos)
       .then(repoData => res.json(repoData))
