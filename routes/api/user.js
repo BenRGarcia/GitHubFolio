4e81040f243446d5213be68dde37e0a0c493f5ed
Ben Garcia <ben@seebenprogram.com>
2018-06-16 20:34:26 -0400

    relocate file upload

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,23 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const path = require('path')
-const multer = require('multer')
 const { getPinnedRepos } = require('../../utils/githubAPI')
 const isAuthenticated = require('../../utils/isAuthenticated')
 const User = require('../../controllers/UserController')
 const PinnedRepos = require('../../controllers/PinnedRepositoryController')
-
-// Set storage engine
-const storage = multer.diskStorage({
-  destination: path.join(__dirname, '../../temp/photos/'),
-  filename: (req, file, cb) => {
-    const fileName = file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-    cb(null, fileName)
-  }
-})
-
-// Init upload
-const upload = multer({ storage }).single('profile')
+const handleUpload = require('../../utils/imageUpload')
 
 /**
  * API Routes - '/api/user'
@@ -45,7 +32,6 @@ router.route('/pinnedrepos')
     getPinnedRepos(req.user.accessToken)
       .then(repos => PinnedRepos.bulkCreate({ _id: req.user._id }, repos))
       .then(() => res.status(201).send())
-      .catch(err => next(err))
   })
   // Update user pinned repos
   .put(isAuthenticated, (req, res, next) => {
@@ -57,21 +43,9 @@ router.route('/pinnedrepos')
 router.route('/photo')
   // Add photo to pinned repo
   .post(/* isAuthenticated, */ (req, res, next) => {
-    upload(req, res, (err) => {
-      if (err) {
-        res.json({ err })
-      } else {
-        console.log(req.file)
-        res.send('test')
-      }
-    })
-    /* PinnedRepos.addPhoto({})
-      .then(() => res.status(201).send())
-      .catch(err => next(err)) */
-    /* res.json({
-      message: 'got it',
-      body: req.body
-    }) */
+    handleUpload(req, res)
+      .then(response => res.json(response))
+      .catch(err => next(err))
   })
 
 module.exports = router
