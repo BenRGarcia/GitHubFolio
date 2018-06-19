49ed480c2d795d664d96f8e18dd353ab8543edc6
Ben Garcia <ben@seebenprogram.com>
2018-06-19 12:45:55 -0400

    change unauth to redirect '/'

--- utils/isAuthenticated.js
+++ utils/isAuthenticated.js
@@ -2,7 +2,6 @@ module.exports = (req, res, next) => {
   if (req.isAuthenticated()) {
     next()
   } else {
-    res.status(401).json({ message: 'UNAUTHORIZED' })
-    // res.status(401).redirect('/')
+    res.status(401).redirect('/')
   }
 }
