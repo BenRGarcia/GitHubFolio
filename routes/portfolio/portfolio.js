d636e2388fe260538fc0cce1d56732da71c1a6fc
Ben Garcia <ben@seebenprogram.com>
2018-06-19 14:46:43 -0400

    add pseudocode

--- routes/portfolio/portfolio.js
+++ routes/portfolio/portfolio.js
@@ -5,8 +5,35 @@ const router = express.Router()
  * Public Routes - '/portfolio'
  */
 
-// Use React Server Side Rendering
-router.route('/')
+/**
+ * Psuedocode:
+ *   1) Endpoint receives a GET request
+ *   2) Extract req param
+ *   3) Grab data about user from DB
+ *   4) Feed data into component
+ *   5) Work some SSR magic
+ *   6) Send response of static assets
+ */
+
+// React Server Side Rendering, send fully rendered page
+router.route('/user/:gitHubId')
+  .get((req, res, next) => {
+    const { gitHubId } = req.param
+    res.send(`Got 'emmmmmmmm`)
+  })
+
+/**
+ * Psuedocode:
+ *   1) Endpoint receives a GET request
+ *   2) Extract req param
+ *   3) Grab data about user from DB
+ *   4) Feed data into component
+ *   5) Work some SSR magic
+ *   6) Create zip file with rendered output
+ *   7) Send response of zip file
+ */
+
+router.route('/ssr/:gitHubId')
   .get((req, res, next) => {
     res.send(`Got 'emmmmmmmm`)
   })
