6a5ceffebbf6f42bf2c02fbac0132d03388f39b6
Ben Garcia <ben@seebenprogram.com>
2018-06-19 14:21:53 -0400

    add portfolio route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 const apiRoutes = require('./api')
+const portfolioRoutes = require('./portfolio/portfolio')
 
 // API Routes
 router.use('/api', apiRoutes)
+
+// Public Portfolio Routes
+router.use('/portfolio', portfolioRoutes)
 
 module.exports = router
