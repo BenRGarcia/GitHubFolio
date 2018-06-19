b5947a4960bc16186b41d014ab4a78856c119991
Ben Garcia <ben@seebenprogram.com>
2018-06-19 09:47:56 -0400

    add image name to schema

--- models/PinnedRepositories.js
+++ models/PinnedRepositories.js
@@ -2,29 +2,40 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const PinnedRepositoriesSchema = new Schema({
+  // Name of repo
   name: {
     type: String,
     required: true,
     minlength: 1,
     trim: true
   },
+  // Description of repo
   description: {
     type: String,
     required: false,
     trim: true
   },
+  // GitHub repo home page
   homepageUrl: {
     type: String,
     required: false,
     trim: true
   },
+  // URL of deployed site
   url: {
     type: String,
     required: true,
     minlength: 1,
     trim: true
   },
+  // Thumbnail photo public URL
   imageUrl: {
+    type: String,
+    minlength: 1,
+    required: false
+  },
+  // Image file name
+  imageName: {
     type: String,
     minlength: 1,
     required: false
