5dc62aebc5e03741fa9ffe7f26be2d7f75447151
Ben Garcia <ben@seebenprogram.com>
2018-06-19 15:40:53 -0400

    temp stash

--- routes/portfolio/portfolio.js
+++ routes/portfolio/portfolio.js
@@ -1,6 +1,26 @@
 const express = require('express')
 const router = express.Router()
 const { findOneByGitHubId } = require('../../controllers/UserController')
+const React = require('react')
+const { renderToStaticMarkup } = require('react-dom/server')
+
+// Jumble everything into one file to get it working, will modularize later
+const template = (title) => (
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My Portfolio Site</title>
+</head>
+<body>
+  <div id='app'>
+    ${title}
+  </div>
+</body>
+</html>`
+)
 
 /**
  * Public Routes - '/portfolio'
@@ -19,10 +39,11 @@ const { findOneByGitHubId } = require('../../controllers/UserController')
 // React Server Side Rendering, send fully rendered page
 router.route('/user/:gitHubId')
   .get((req, res, next) => {
-    const { gitHubId } = req.params
-    findOneByGitHubId({ gitHubId })
-      .then(userData => res.json(userData))
-      .catch(err => res.json(err))
+
+    // const { gitHubId } = req.params
+    // findOneByGitHubId({ gitHubId })
+    //   .then(userData => res.json(userData))
+    //   .catch(err => res.json(err))
   })
 
 /**
