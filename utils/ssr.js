64aeb32cf7ff238cfa7e0821f21ba23d2310e27f
Ben Garcia <ben@seebenprogram.com>
2018-06-19 19:30:56 -0400

    Delete mock CSS data

--- utils/ssr.js
+++ utils/ssr.js
@@ -1,36 +1,34 @@
 const React = require('react')
 const createReactClass = require('create-react-class')
 
-const style = {
-  background: 'red'
-}
+// const user = { template, color, pinnedRepositories, bio, displayName, email, location, photo, profileUrl }
 
 module.exports = createReactClass({
   render () {
     return (
       <html lang="en">
         <head>
-          <meta charset="UTF-8" />
+          <meta charSet="UTF-8" />
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <meta
-            http-equiv="X-UA-Compatible"
+            httpEquiv="X-UA-Compatible"
             content="ie=edge"
           />
           <link
             rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
             integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
-            crossorigin="anonymous"
+            crossOrigin="anonymous"
           />
           <title>
             My Portfolio Site
           </title>
         </head>
-        <body style={style}>
-          <div id='app'>
+        <body>
+          <div className='container text-center'>
             <h1>
               Hello World!
             </h1>
