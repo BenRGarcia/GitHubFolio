a5453bf5bb3c5722de9cf0bd5dde2736ac511285
Ben Garcia <ben@seebenprogram.com>
2018-06-21 20:18:55 -0400

    refactor for simplicity

--- utils/isAuthenticated.js
+++ utils/isAuthenticated.js
@@ -4,7 +4,7 @@ module.exports = {
       ? next()
       : res.status(401).redirect('/')
   },
-  isAuthenticatedBoolean: (req, res, next) => {
+  isAuthenticatedBoolean: (req) => {
     return !!req.isAuthenticated()
   }
 }
