5d1adb935a68c872e0c064fafa15a28f45cff57d
Ben Garcia <ben@seebenprogram.com>
2018-06-22 15:38:07 -0400

    more success, delete comments

--- controllers/ssr.js
+++ controllers/ssr.js
@@ -8,14 +8,10 @@ const minimalistTemplate = require('./ssrMinimalistTemplate')
 const stylizedTemplate = require('./ssrStylizedTemplate')
 
 const wrapInHtmlShell = ({ body }) => {
-  // console.log(`wrapInHtmlShell() was called`)
-  // console.log(`will attempt to put body inside:\n`, body)
   return React.createElement(htmlShell, { body })
 }
 
 const generateHTMLWithChosenTemplate = ({ userData }) => {
-  // console.log(`generateHTMLwithChosenTemplate() was called`)
-  // console.log(`which template?\n`, userData.chosenTemplate)
   return userData.chosenTemplate === 'stylized'
     ? React.createElement(stylizedTemplate, { userData })
     : React.createElement(minimalistTemplate, { userData })
@@ -30,15 +26,9 @@ const renderPortfolioBody = ({ userData }) => {
 const renderPortfolioPage = ({ userData }) => {
   const body = generateHTMLWithChosenTemplate({ userData })
   const bodyRendered = renderToStaticMarkup(body)
-  // console.log(`\n====\nline 33 here is the body:\n`, bodyRendered)
-  // return bodyRendered // -> looks beautiful right here
-  const page = wrapInHtmlShell({ body: bodyRendered })
-  const renderedHTML = renderToStaticMarkup(page)
-  console.log(renderedHTML)
-  return renderedHTML
-  // const renderedHTML = renderToStaticMarkup(page)
-  // console.log(`\n====\nhere's the whole dang thing:\n<!DOCTYPE html>${renderedHTML}`)
-  // return `<!DOCTYPE html>${renderedHTML}`
+  const html = wrapInHtmlShell({ body: bodyRendered })
+  const renderedHTML = renderToStaticMarkup(html)
+  return `<!DOCTYPE html>${renderedHTML}`
 }
 
 module.exports = {
