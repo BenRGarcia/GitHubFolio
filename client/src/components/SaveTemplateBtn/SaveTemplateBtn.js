f7df5fecdbd94648f0487a0104ac0adc10b2ab7e
dtroxler21 <dtroxler21@gmail.com>
2018-06-23 12:51:23 -0400

    working on saving a template

--- client/src/components/SaveTemplateBtn/SaveTemplateBtn.js
+++ client/src/components/SaveTemplateBtn/SaveTemplateBtn.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import "./SaveTemplateBtn.css";
+import { connect } from "react-redux";
+import { fetchUserInfo, editUserInfo } from '../../actions/index';
+import { bindActionCreators } from "redux";
 
-export const SaveTemplateBtn = props => {
-  return(
-    <div className="save-button pt-4 pb-4">
-      <a href='#'><button className='btn btn-dark'>Save Template</button></a>
-    </div>
-  )
+
+export class SaveTemplateBtn extends Component {
+  state = {
+    template: " "
+  }
+
+  initialized = false;
+
+  componentWillReceiveProps(nextProps){
+    if(!this.initialized){
+      this.initialized = true;
+      const { template } = nextProps.userInfo
+      this.setState({
+        template
+      })
+    }
+  }
+
+  handleSubmit = (e) => {
+      e.preventDefault()
+      this.props.editUserInfo(this.state).then(response => {console.log(response)}) 
+    }
+
+
+  render() {
+    return(
+      <div className="save-button pt-4 pb-4">
+        <button className='btn btn-dark' onClick={this.handleSubmit}>Save Template</button>
+      </div>
+    )
+  }
 }
+
+const mapStateToProps = state => {
+  return { userInfo: state.userInfo };
+};
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ fetchUserInfo, editUserInfo }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SaveTemplateBtn);
 
--- client/src/components/TemplatePage/TemplatePage.js
+++ client/src/components/TemplatePage/TemplatePage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./TemplatePage.css";
-// import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import  { SaveTemplateBtn } from "../../components/SaveTemplateBtn/SaveTemplateBtn";
 import  { TemplateToggleBtns } from "../../components/TemplateToggleBtns/TemplateToggleBtns";
 
