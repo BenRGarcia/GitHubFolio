0c8c8be5bfb7ebb4a728bfe2fca42a2ad2cb5754
Melissa Burnham <melissarburnham@gmail.com>
2018-06-22 20:41:55 -0400

    nothing changed

--- client/src/components/DashboardSidebar/DashboardSidebar.js
+++ client/src/components/DashboardSidebar/DashboardSidebar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./DashboardSidebar.css";
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 export const DashboardSidebar = props => (
   <div className="sidenav">
