20b0be6b670ed9e9c5d1d63931a87e699ce0fe68
Jason M. Aron <jasonaronart@gmail.com>
2018-06-24 18:51:56 -0400

    navbar css

--- client2/src/components/DashboardSidebar/LogoSidebar.js
+++ client2/src/components/DashboardSidebar/LogoSidebar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
+const logoSizing = {
+width: '50px',
+height: 'auto',
+display: 'block',
+margin: 'auto'
+}
+
 const LogoSidebar = () => (
   <div className='d-flex justify-content-center my-5 py-4'>
     <img
       className='img-fluid'
+      style={ logoSizing }
       src={require('../../images/githubfolio-gradient.png')}
       alt="GitHubFolio logo"
     />
--- client2/src/components/DashboardSidebar/LogoutSidebar.js
+++ client2/src/components/DashboardSidebar/LogoutSidebar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
+const logoutLinks = {
+  fontFamily: 'Roboto sans-serif',
+  fontSize: '18px',
+  color: '#979799',
+  paddingTop: '15px',
+  paddingLeft: '25px',
+  height: '50px',
+  width: '100%',
+}
+
 const Logout = () => (
-  <li className="nav-item text-center mt-5">
+  <li className="nav-item my-2">
     <a
       href="/api/auth/logout"
-      className='nav-link'
+      className='nav-link ml-2'
+      style={ logoutLinks }
     >
       <i
-        className='fas fa-sign-out-alt mx-1'
+        className='fas fa-sign-out-alt mx-1 mr-4'
       ></i>
       Logout
     </a>
