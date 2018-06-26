ea8010229155568d1e7e8c3b9e0609b26d8f74e4
Ben Garcia <ben@seebenprogram.com>
2018-06-26 11:51:21 -0400

    accordion success

--- client2/src/components/Dashboard/EditRepos.js
+++ client2/src/components/Dashboard/EditRepos.js
@@ -6,72 +6,84 @@ const EditRepos = ({ repositories, handleChange, handleSubmit }) => {
   return repositories.map(repo => {
     const { _id, name, /* imageUrl, */ description, deployedUrl } = repo;
     return (
-      <fieldset key={_id} className='card mb-4'>
-        <div className="card-header">
-          Pinned Repository #{++counter}
+      <fieldset key={_id} className='card'>
+        <div className="card-header" id="headingOne">
+          <button
+            class="btn btn-link"
+            type="button"
+            data-toggle="collapse"
+            data-target={`#collapse${_id}`}
+            aria-expanded="true"
+            aria-controls="collapseOne"
+          >
+            Pinned Repository #{++counter}
+          </button>
         </div>
-        <div className="card-body py-2">
-          {/* Repo name template */}
-          <div className="form-group mb-2 row">
-            <label
-              className='col-sm-2 col-form-label text-left pb-1 pt-0'
-            >
-              Repository Name:
+        <div
+          id={`collapse${_id}`}
+          class="collapse"
+          aria-labelledby="headingOne"
+          data-parent="#accordionExample"
+        >
+          <div className="card-body py-2">
+            {/* Repo name template */}
+            <div className="form-group mb-2 row">
+              <label
+                className='col-sm-2 col-form-label text-left pb-1 pt-0'
+              >
+                Repository Name:
           </label>
-            <div className='col-sm-10'>
-              <input
-                name='name'
-                type="text"
-                value={name || ''}
-                onChange={e => handleChange(e, _id)}
-                className='form-control'
-              />
+              <div className='col-sm-10'>
+                <input
+                  name='name'
+                  type="text"
+                  value={name || ''}
+                  onChange={e => handleChange(e, _id)}
+                  className='form-control'
+                />
+              </div>
             </div>
-          </div>
-          {/* Deployed Website template */}
-          <div className="form-group mb-2 row">
-            <label
-              className='col-sm-2 col-form-label text-left pb-1 pt-0'
-            >
-              Deployed Site:
+            {/* Deployed Website template */}
+            <div className="form-group mb-2 row">
+              <label
+                className='col-sm-2 col-form-label text-left pb-1 pt-0'
+              >
+                Deployed Site:
             </label>
-            <div className='col-sm-10'>
-              <input
-                name='deployedUrl'
-                type="text"
-                value={deployedUrl || ''}
-                onChange={e => handleChange(e, _id)}
-                className='form-control'
-              />
+              <div className='col-sm-10'>
+                <input
+                  name='deployedUrl'
+                  type="text"
+                  value={deployedUrl || ''}
+                  onChange={e => handleChange(e, _id)}
+                  className='form-control'
+                />
+              </div>
             </div>
-          </div>
-          {/* Description template */}
-          <div className="form-group mb-2 row">
-            <label
-              className='col-sm-2 col-form-label text-left pb-1 pt-0'
-            >
-              Description:
+            {/* Description template */}
+            <div className="form-group mb-2 row">
+              <label
+                className='col-sm-2 col-form-label text-left pb-1 pt-0'
+              >
+                Description:
             </label>
-            <div className='col-sm-10'>
-              <textarea
-                name='description'
-                type="text"
-                value={description || ''}
-                onChange={e => handleChange(e, _id)}
-                className='form-control'
-                rows='2'
-              />
+              <div className='col-sm-10'>
+                <textarea
+                  name='description'
+                  type="text"
+                  value={description || ''}
+                  onChange={e => handleChange(e, _id)}
+                  className='form-control'
+                  rows='2'
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <button type='submit' className='btn btn-outline-dark'>
-              Save Changes
+            <div>
+              <button type='submit' className='btn btn-outline-dark'>
+                Save Changes
             </button>
+            </div>
           </div>
-        </div>
-        <hr className='my-0'/>
-        <div className="card-body">
-          image upload stuff
         </div>
       </fieldset>
     );
--- client2/src/pages/Dashboard/DashboardEditRepos.js
+++ client2/src/pages/Dashboard/DashboardEditRepos.js
@@ -33,16 +33,13 @@ class DashboardEditRepos extends Component {
       <div className="row mt-3">
         <div className="col">
           <form onSubmit={this.handleSubmit}>
-            <div className="accordion">
+            <div className="accordion" id='accordionExample'>
               <EditRepos
                 repositories={this.state.repositories}
                 handleChange={this.handleChange}
                 handleSubmit={this.handleSubmit}
               />
             </div>
-            <button type='submit' className='mb-3 btn btn-outline-dark'>
-              Save Changes
-            </button>
           </form>
         </div>
       </div>
