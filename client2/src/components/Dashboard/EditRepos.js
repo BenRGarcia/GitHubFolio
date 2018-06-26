1491cee17d116f6743d38cd51080da3dff11fc84
Ben Garcia <ben@seebenprogram.com>
2018-06-25 21:17:16 -0400

    comment out errors

--- client2/src/components/Dashboard/EditRepos.js
+++ client2/src/components/Dashboard/EditRepos.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { UserInput } from '../';
+// import { UserInput } from '../';
 
 const EditRepos = ({ repositories, handleChange }) => (
   <React.Fragment>
