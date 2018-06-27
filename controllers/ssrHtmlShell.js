e56040a23b618decbba891dc706c83cef9d5484e
Ben Garcia <ben@seebenprogram.com>
2018-06-27 17:59:05 -0400

    add font awesome

--- controllers/ssrHtmlShell.js
+++ controllers/ssrHtmlShell.js
@@ -17,6 +17,12 @@ module.exports = createReactClass({
           />
           <link
             rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.1.0/css/all.css"
+            integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt"
+            crossorigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
             href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css"
             integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
             crossOrigin="anonymous"
