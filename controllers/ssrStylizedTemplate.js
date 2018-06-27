487c14f79b3534e2a4401f21545ed068e513f4be
Melissa Burnham <melissarburnham@gmail.com>
2018-06-27 19:32:28 -0400

    add navbar and topPanel styling

--- controllers/ssrStylizedTemplate.js
+++ controllers/ssrStylizedTemplate.js
@@ -6,7 +6,8 @@ const style = {
   styleTitleName: {
     fontFamily: 'Oswald-SemiBold',
     fontSize: '36px',
-    color: '#3a4145'
+    color: '#3a4145',
+    paddingTop: '140px'
   },
 
   styleBio: {
@@ -21,32 +22,57 @@ const style = {
 
   topPanel: {
     width: '100vw',
-    marginLeft: 'calc(-50vw + 50%)'
+    marginLeft: 'calc(-50vw + 50%)',
+    // height: '300px'
+  },
+
+  profilePic: {
+    float: 'none',
+    position: 'absolute',
+    top: '5%',
+    left: '50%',
+    transform: 'translate(-50%, -10%)',
+    width: '150px'
   }
-}
+};
 
 module.exports = createReactClass({
   render () {
     const { repositories, bio, profileName, email, location, profileImageUrl, profilePageUrl } = this.props.userData
     return (
-
       <div className='container text-center'>
+        <nav className="navbar navbar-expand-lg navbar-light bg-white nav_font border-bottom navbar-fixed-bottom ">
+          <a class="navbar-brand" href="#">{profileName}</a>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item pr-4">
+                <a href={profilePageUrl} target="_blank">
+                  <i className="fab fa-github-square fa-2x"></i>
+                </a>
+              </li>
+              <li className="nav-item pr-4">
+                <a href={`mailto:${email}`} target="_top">
+                  <i className=" fab fas fa-envelope-square fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </nav>
         <div className='section bg-light pb-5 pt-2' style={style.topPanel}>
-          <div><img src={profileImageUrl} alt="Profile"
+          <div><img src={profileImageUrl} alt="Profile" 
             className='rounded-circle mx-auto d-block justify-content-center mt-2 mb-2 border bg-white p-2'
-            style={{ width: '150px' }} />
+            style={style.profilePic} />
           </div>
           <h1 style={style.styleTitleName}>{profileName}</h1>
           <p style={style.styleBio}>{location}</p>
           <h5 style={style.styleBio}> {bio}</h5>
-          <div className='row justify-content-center'>
-            <a href={profilePageUrl} target='_blank'>
-              <i class="fab fa-github-square fa-2x pr-2"></i>
-            </a>
-            <a href={`mailto:${email}`} target='_top' className='secondary'>
-              <i class='fab fas fa-envelope-square fa-2x pl-2'></i>
-            </a>
-          </div>
         </div>
 
         <div className="row">
