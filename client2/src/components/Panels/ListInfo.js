979b9c179e361487a533387901b4bcaccce9fd6c
dtroxler21 <dtroxler21@gmail.com>
2018-06-27 21:27:31 -0400

    icons on features

--- client2/src/components/Panels/ListInfo.js
+++ client2/src/components/Panels/ListInfo.js
@@ -11,24 +11,24 @@ const listFontStyle = {
   fontSize: '18px',
   color: '#D8D8D8',
   letterSpacing: '-0.05px',
-  textAlign: 'left'
+
+  margin: '0 auto',
+
 }
 
 
 
 const ListInfo = () => (
-  <div className="row text-white">
+  <div className="row text-white center-text">
     <div className="col-12">
       <h2 className='display-4 mb-4' style={ listTitleFontStyle }>Everything you need for your portfolio</h2>
     </div>
-    <div className="col-12 col-md-8 offset-md-2 text-left" style={ listFontStyle }>
-      <p>Retrieve your pinned repositories from GitHub.</p>
-      <p>Edit your personal information and mission.</p>
-      <p>Edit your repository information and description for your portfolio.</p>
-      <p>Upload photos to accompany your repositories.</p>
-      <p>Choose a beautiful template and preview your portfolio.</p>
-      <p>Receive access to a public URL for your portfolio.</p>
-      <p>Download a file of your portfolio code to edit.</p>
+    <div className="col-12" style={ listFontStyle }>
+      <p><i className='fas fa-code-branch mx-1 mr-3'></i>Retrieve your pinned repositories from GitHub.</p>
+      <p><i className='fas fa-file-alt mx-1 mr-3'></i>Choose a beautiful template and preview your portfolio.</p>
+      <p><i className='far fa-edit mx-1 mr-3'></i>Edit your personal information and mission.</p>
+      <p><i className='fas fa-wrench mx-1 mr-3'></i>Edit your repository information and upload photos.</p>
+      <p><i className='fas fa-th-large mx-1 mr-3'></i>Receive a public URL and download the source code.</p>
     </div>
   </div>
 );
