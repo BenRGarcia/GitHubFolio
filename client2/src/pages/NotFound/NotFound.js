138be7ce378be5cd9ec8d9707e498ceef3dcd30e
Ben Garcia <ben@seebenprogram.com>
2018-06-27 21:27:38 -0400

    404 redirect after 3 seconds

--- client2/src/pages/NotFound/NotFound.js
+++ client2/src/pages/NotFound/NotFound.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Panel } from '../../components';
+import { Redirect } from 'react-router-dom';
 
 const landingBG = {
   backgroundImage: `url(${require("../../images/404Bg.png")})`,
@@ -10,33 +11,53 @@ const landingBG = {
 };
 
 const style404 = {
-  // fontFamily: 'Oswald-medium'
   fontWeight: '700'
 };
 
-const NotFound = () => (
+class NotFound extends Component {
+  state = {
+    redirect: false
+  };
 
-  <Panel style={landingBG}>
-    <div className='row text-center'>
-      <div className="col-12">
-        <h1 className='display-4'>
-          Whoops!
-        </h1>
+  componentDidMount() {
+    this.setState({
+      redirect: false
+    }, () => setTimeout(() => {
+        this.setState({ redirect: true })
+    }, 3000))
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.redirect
+          ?
+          <Redirect to="/" />
+          :
+            <Panel style={landingBG}>
+              <div className='row text-center'>
+                <div className="col-12">
+                  <h1 className='display-4'>
+                    Whoops!
+                  </h1>
+                </div>
+                <div className="col-12">
+                  <h2>
+                    We couldn't find the page you were looking for
+                  </h2>
+                </div>
+                <div className="col-12">
+                  <h1 className='display-1' style={style404}>
+                    404
+                  </h1>
+                </div>
+              </div>
+            </Panel>
+        }
       </div>
-      <div className="col-12">
-        <h2>
-          We couldn't find the page you were looking for
-        </h2>
-      </div>
-      <div className="col-12">
-        <h1 className='display-1' style={style404}>
-          404
-        </h1>
-      </div>
-    </div>
-  
-    
-  </Panel>
-)
+    );
+  }
+}
 
 export default NotFound
