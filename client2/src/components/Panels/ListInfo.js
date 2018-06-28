3536ccdd6d8c7a81e0bcdf2c48fd451393ca212f
Melissa Burnham <melissarburnham@gmail.com>
2018-06-28 10:30:17 -0400

    change font size

--- client2/src/components/Panels/ListInfo.js
+++ client2/src/components/Panels/ListInfo.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const listTitleFontStyle = {
   fontFamily: 'Roboto-Regular',
-  fontSize: '36px',
+  fontSize: '45px',
   letterSpacing: '-0.08px'
 }
 
 const listFontStyle = {
   fontFamily: 'Oswald-Light',
-  fontSize: '18px',
+  fontSize: '22px',
   color: '#D8D8D8',
   letterSpacing: '-0.05px',
   textAlign: 'left',
@@ -30,7 +30,7 @@ const ListInfo = () => (
   <div className = 'container' style= {listFontStyle}> 
     <div className = 'row'>
       <div className = 'col-2'>
-        <p><i className='fas fa-code-branch mx-1 mr-3'></i></p>
+        <p><i className='fas fa-code-branch mx-3 mr-5'></i></p>
       </div>
       <div className = 'col-10'>
         <p>Retrieve your pinned repositories from GitHub.</p>
@@ -38,7 +38,7 @@ const ListInfo = () => (
     </div>
     <div className = 'row'>
       <div className = 'col-2'>
-        <p><i className='fas fa-file-alt mx-1 mr-3'></i></p>
+        <p><i className='fas fa-file-alt mx-3 mr-5'></i></p>
       </div>
       <div className = 'col-10'>
         <p>Choose a beautiful template and preview your portfolio.</p>
@@ -46,7 +46,7 @@ const ListInfo = () => (
     </div>
     <div className = 'row'>
       <div className = 'col-2'>
-        <p><i className='far fa-edit mx-1 mr-3'></i></p>
+        <p><i className='far fa-edit mx-3 mr-5'></i></p>
       </div>
       <div className = 'col-10'>
         <p>Edit your personal information and mission.</p>
@@ -54,7 +54,7 @@ const ListInfo = () => (
     </div>
     <div className = 'row'>
       <div className = 'col-2'>
-        <p><i className='fas fa-wrench mx-1 mr-3'></i></p>
+        <p><i className='fas fa-wrench mx-3 mr-5'></i></p>
       </div>
       <div className = 'col-10'>
         <p>Edit your repository information and upload photos.</p>
@@ -62,7 +62,7 @@ const ListInfo = () => (
     </div>
     <div className = 'row'>
       <div className = 'col-2'>
-        <p><i className='fas fa-th-large mx-1 mr-3'></i></p>
+        <p><i className='fas fa-th-large mx-3 mr-5'></i></p>
       </div>
       <div className = 'col-10'>
         <p>Receive a public URL and download the source code.</p>
